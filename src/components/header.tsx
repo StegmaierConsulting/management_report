a563571041ef744c80200cd182b746fc115b9bf5
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-07-11 20:12:29 -0400

    header component and its stylization were modified
    The improvement of the component includes logo implementation with the possibility of redirection to the main page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,15 @@
 "use client";
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/header-menu";
+import { HoveredLink, Menu, MenuItem } from "@/components/ui/header-menu";
 import { cn } from "@/utils/cn";
 import '@/app/globals.css';
+import Link from "next/link";
+import Image from "next/image";
 
 export function Header() {
   return (
-    <div className="relative w-full flex items-center justify-center">
-      <Navbar className="top-2 " />
-      <p className="text-black dark:text-white ">
-        The Navbar will show on top of the page
-      </p>
+    <div className="relative w-full flex items-center justify-center bg-black">
+      <Navbar className="top-2" />
     </div>
   );
 }
@@ -18,23 +17,32 @@ export function Header() {
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
-    <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-    >
-      <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Formularios">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/ZeroTolerance">Tolerancia Cero</HoveredLink>
-            <HoveredLink href="/IncidentOrAccident">Incidente o Accidente</HoveredLink>
-          </div>
-        </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Buscador">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Condición Subestándar</HoveredLink>
-            <HoveredLink href="/seo">Incidente o Accidente</HoveredLink>
-          </div>
-        </MenuItem>
-      </Menu>
+    <div className={cn("fixed top-5 inset-x-0 max-w-4xl mx-auto z-50", className)}>
+      <div className="flex items-center space-x-4 bg-white p-2 rounded-full ">
+        <Link href="/main">
+          <Image
+            src="/stegmaier.png" // Cambia este src por la ruta de tu imagen
+            alt="Logo"
+            width={150}
+            height={60}
+            className="cursor-pointer"
+          />
+        </Link>
+        <Menu setActive={setActive}>
+          <MenuItem setActive={setActive} active={active} item="Formularios">
+            <div className="flex flex-col space-y-2 text-sm">
+              <HoveredLink href="/ZeroTolerance">Tolerancia Cero</HoveredLink>
+              <HoveredLink href="/IncidentOrAccident">Incidente o Accidente</HoveredLink>
+            </div>
+          </MenuItem>
+          <MenuItem setActive={setActive} active={active} item="Buscador">
+            <div className="flex flex-col space-y-2 text-sm">
+              <HoveredLink href="/web-dev">Condición Subestándar</HoveredLink>
+              <HoveredLink href="/seo">Incidente o Accidente</HoveredLink>
+            </div>
+          </MenuItem>
+        </Menu>
+      </div>
     </div>
   );
 }
--- src/components/ui/header-menu.tsx
+++ src/components/ui/header-menu.tsx
@@ -26,7 +26,7 @@ export const MenuItem = ({
   children?: React.ReactNode;
 }) => {
   return (
-    <div onMouseEnter={() => setActive(item)} className="relative ">
+    <div onMouseEnter={() => setActive(item)} className="relative">
       <motion.p
         transition={{ duration: 0.3 }}
         className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white"
@@ -71,7 +71,7 @@ export const Menu = ({
   return (
     <nav
       onMouseLeave={() => setActive(null)} // resets the state
-      className="relative rounded-full boder border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6 "
+      className="relative rounded-full border border-transparent bg-white shadow-input flex justify-center space-x-8 px-8 py-4"
     >
       {children}
     </nav>
@@ -114,7 +114,7 @@ export const HoveredLink = ({ children, ...rest }: any) => {
   return (
     <Link
       {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-black "
+      className="text-black dark:text-neutral-200 hover:text-blue-400"
     >
       {children}
     </Link>
