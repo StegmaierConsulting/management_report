ab05caef3f2da98c4c01af94f15ed36307fca989
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-07-23 16:50:15 -0400

    Create Main page Search Zero Toletance form

---
+++ src/pages/SearchZeroTolerance.tsx
@@ -0,0 +1,19 @@
+// pages/search.tsx
+import React from 'react';
+import SearchForms from '@/components/SearchFormZeroTolerance';
+import '@/app/globals.css';
+import { Header } from '@/components/header';
+
+const SearchPage: React.FC = () => {
+  return (
+  <>
+  <Header />
+    <div className="p-4 mt-24">
+      <h1 className="text-2xl text-center text-blue-400 p-2 font-bold mb-4">Buscar y Editar Formularios Tolerancia 0</h1>
+      <SearchForms />
+    </div>
+    </>
+  );
+};
+
+export default SearchPage;
