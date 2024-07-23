a78f6bd7059f72d98460d1f4552135fe6cbb441d
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-07-23 17:29:54 -0400

    fix styles

--- src/pages/SearchZeroTolerance.tsx
+++ src/pages/SearchZeroTolerance.tsx
@@ -8,7 +8,7 @@ const SearchPage: React.FC = () => {
   return (
   <>
   <Header />
-    <div className="p-4 mt-24">
+    <div className="p-4 mt-16">
       <h1 className="text-2xl text-center text-blue-400 p-2 font-bold mb-4">Buscar y Editar Formularios Tolerancia 0</h1>
       <SearchForms />
     </div>
