467590462c75874b593e1e9a2f4c89b65c3f9ccd
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-07-23 12:41:50 -0400

    configure firebase with realtime database, firestore, and storage
    - Initialize Firebase with configuration from environment variables.
    - Set up Realtime Database for reading authentication data.
    - Set up Firestore for storing documents and their metadata.
    - Set up Firebase Storage for uploading and retrieving image files.
    - Configure session persistence for Firebase Authentication.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,24 +2,25 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, setPersistence, browserSessionPersistence } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const storage = getStorage(app);
 const authentication = getAuth(app);
+const firestore = getFirestore(app);
 
-// Configurar la persistencia de sesión en el navegador
 setPersistence(authentication, browserSessionPersistence)
   .then(() => {
     console.log("Persistencia de sesión configurada correctamente.");
@@ -28,4 +29,4 @@ setPersistence(authentication, browserSessionPersistence)
     console.error("Error al configurar la persistencia de sesión:", error);
   });
 
-export { app, database, storage, authentication };
+export { app, database, storage, authentication, firestore };
