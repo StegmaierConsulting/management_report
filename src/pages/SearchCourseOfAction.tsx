c64fc16587572cfcd8a3fb9a6400d3b10fd79c1d
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-08-02 18:40:03 -0400

    Add MainPage component with search and edit functionality
    Implemented the MainPage component as the main entry point of the application.
    Included the Header component for consistent navigation across the app.
    Added a centered main title describing the page's functionality.
    Integrated the SearchForms component for searching and editing action plan forms.
    Ensured consistent styling by importing global styles from globals.css.

---
+++ src/pages/SearchCourseOfAction.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchForms from '@/components/SearchCoursOfActionForm';
+import '@/app/globals.css';
+import { Header } from '@/components/header';
+
+const MainPage: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        <h1 className="text-3xl text-center my-8">Búsqueda y Edición de Formularios</h1>
+        <SearchForms />
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
