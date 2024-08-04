69a6e624b91f2447919aafc40eb8e10e829b564f
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-08-04 18:42:37 -0400

    Add DescriptionAccidentIncident component for accident/incident cause descriptions
    Implemented DescriptionAccidentIncident component for detailing the causes of accidents/incidents.
    The component includes fields for immediate causes (substandard acts, substandard conditions, and external conditions) and basic causes (personal factors, work factors, and external factors).
    Utilizes local state (formData) to manage input values.
    Each cause type is represented with input fields styled with appropriate borders and highlighting for easy user interaction.

---
+++ src/components/ConcludingReportComponents/DescriptionAccidentIncident.tsx
@@ -0,0 +1,128 @@
+// AccidentCausesForm.tsx
+
+import React, { useState } from 'react';
+
+interface CausesData {
+  actoSubestand: string;
+  condicionSubestand: string;
+  condicionExterna: string;
+  factoresPersonales: string;
+  factoresTrabajo: string;
+  factorExterno: string;
+}
+
+const DescriptionAccidentIncident: React.FC = () => {
+  const [formData, setFormData] = useState<CausesData>({
+    actoSubestand: '',
+    condicionSubestand: '',
+    condicionExterna: '',
+    factoresPersonales: '',
+    factoresTrabajo: '',
+    factorExterno: '',
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <div className=" p-4 mx-14 mb-4">
+      <h2 className="text-2xl font-bold mb-4">10. DESCRIPCIÓN DE LAS CAUSAS DEL ACCIDENTE/INCIDENTE</h2>
+      
+      <h3 className="text-xl font-bold mb-2 underline">Causas Inmediatas:</h3>
+      <p className="mb-4 font-semibold text-lg">Las causas inmediatas de la ocurrencia de este accidente/incidente son:</p>
+      <table className="table-auto w-full mb-8">
+        <tbody>
+          <tr className="">
+            <td className="p-2 font-bold">Acto subestándar</td>
+            <td className="p-2">:</td>
+            <td className="p-2">
+              <input
+                type="text"
+                name="actoSubestand"
+                value={formData.actoSubestand}
+                onChange={handleInputChange}
+                className="w-full p-1 border-b border-gray-300 focus:outline-none text-[#0070c0] font-bold"
+              />
+            </td>
+          </tr>
+          <tr className="">
+            <td className="p-2">Condición subestándar</td>
+            <td className="p-2">:</td>
+            <td className="p-2">
+              <input
+                type="text"
+                name="condicionSubestand"
+                value={formData.condicionSubestand}
+                onChange={handleInputChange}
+                className="w-full p-1 border-b border-gray-300 focus:outline-none text-[#0070c0] font-bold"
+              />
+            </td>
+          </tr>
+          <tr className="">
+            <td className="p-2">Condición externa</td>
+            <td className="p-2">:</td>
+            <td className="p-2">
+              <input
+                type="text"
+                name="condicionExterna"
+                value={formData.condicionExterna}
+                onChange={handleInputChange}
+                className="w-full p-1 border-b border-gray-300 focus:outline-none text-[#0070c0] font-bold"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h3 className="text-xl font-bold mb-2 underline">Causas Básicas:</h3>
+      <p className="mb-4 text-lg font-semibold">Las causas básicas de la ocurrencia de este accidente/incidente son:</p>
+      <table className="table-auto w-full">
+        <tbody>
+          <tr className="">
+            <td className="p-2">Factores Personales</td>
+            <td className="p-2">:</td>
+            <td className="p-2">
+              <input
+                type="text"
+                name="factoresPersonales"
+                value={formData.factoresPersonales}
+                onChange={handleInputChange}
+                className="w-full p-1 border-b border-gray-300 focus:outline-none text-[#0070c0] font-bold"
+              />
+            </td>
+          </tr>
+          <tr className="">
+            <td className="p-2">Factores del Trabajo</td>
+            <td className="p-2">:</td>
+            <td className="p-2">
+              <input
+                type="text"
+                name="factoresTrabajo"
+                value={formData.factoresTrabajo}
+                onChange={handleInputChange}
+                className="w-full p-1 border-b border-gray-300 focus:outline-none text-[#0070c0] font-bold"
+              />
+            </td>
+          </tr>
+          <tr className="">
+            <td className="p-2">Factor Externo</td>
+            <td className="p-2">:</td>
+            <td className="p-2">
+              <input
+                type="text"
+                name="factorExterno"
+                value={formData.factorExterno}
+                onChange={handleInputChange}
+                className="w-full p-1 border-b border-gray-300 focus:outline-none text-[#0070c0] font-bold"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DescriptionAccidentIncident;
