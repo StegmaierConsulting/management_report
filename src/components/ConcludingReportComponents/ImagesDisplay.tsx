2fcdd1401a8e092c4f6af55783dd162b197b3805
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-08-04 18:40:38 -0400

    Add ImagesDisplay component for displaying images
    Implemented ImagesDisplay component to showcase a grid of images.
    The component accepts an array of image URLs (imageUrls) as a prop.
    Images are displayed in a responsive grid layout, adjusting from 1 column on small screens to 4 columns on larger screens.
    Each image is displayed with a border and proper alignment.
    A fallback message is shown if there are no images to display, ensuring clarity in the UI.

---
+++ src/components/ConcludingReportComponents/ImagesDisplay.tsx
@@ -0,0 +1,28 @@
+// Componente ImagesDisplay
+import React from 'react';
+
+interface ImagesDisplayProps {
+  imageUrls: string[];
+}
+
+const ImagesDisplay: React.FC<ImagesDisplayProps> = ({ imageUrls }) => {
+  return (
+    <div className="p-4 mx-16 mb-4">
+      <h2 className="text-lg font-bold mb-4">15. FOTOS EXPLICATIVAS:</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {imageUrls.length > 0 ? (
+          imageUrls.map((url, index) => (
+            <div key={index} className="flex justify-center">
+              <img src={url} alt={`Imagen ${index + 1}`} className="max-w-full h-auto border-2 border-gray-300" crossOrigin="anonymous" />
+            </div>
+          ))
+        ) : (
+          <div className="col-span-4 text-center text-gray-500">No hay imágenes para mostrar</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ImagesDisplay;
+
