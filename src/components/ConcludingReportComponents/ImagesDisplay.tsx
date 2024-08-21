5451397df8e99d9391f524dd273ab3fe6d9d43d8
zury <zurydevs@gmail.com>
2024-08-21 17:27:33 -0400

    fix images display

--- src/components/ConcludingReportComponents/ImagesDisplay.tsx
+++ src/components/ConcludingReportComponents/ImagesDisplay.tsx
@@ -1,4 +1,3 @@
-// Componente ImagesDisplay
 import React from 'react';
 
 interface ImagesDisplayProps {
@@ -9,11 +8,16 @@ const ImagesDisplay: React.FC<ImagesDisplayProps> = ({ imageUrls }) => {
   return (
     <div className="p-4 mx-16 mb-4">
       <h2 className="text-lg font-bold mb-4">15. FOTOS EXPLICATIVAS:</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {imageUrls.length > 0 ? (
           imageUrls.map((url, index) => (
             <div key={index} className="flex justify-center">
-              <img src={url} alt={`Imagen ${index + 1}`} className="max-w-full h-auto border-2 border-gray-300" crossOrigin="anonymous" />
+              <img
+                src={url}
+                alt={`Imagen ${index + 1}`}
+                className="max-w-full max-h-96 w-auto h-auto border-2 border-gray-300"
+                crossOrigin="anonymous"
+              />
             </div>
           ))
         ) : (
@@ -25,4 +29,3 @@ const ImagesDisplay: React.FC<ImagesDisplayProps> = ({ imageUrls }) => {
 };
 
 export default ImagesDisplay;
-
