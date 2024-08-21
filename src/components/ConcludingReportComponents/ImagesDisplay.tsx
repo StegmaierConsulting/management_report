c27089a7281c2a82c4f536f5d0b87dd8160b5ee1
zury <zurydevs@gmail.com>
2024-08-21 17:39:48 -0400

    fix margin display

--- src/components/ConcludingReportComponents/ImagesDisplay.tsx
+++ src/components/ConcludingReportComponents/ImagesDisplay.tsx
@@ -8,14 +8,21 @@ const ImagesDisplay: React.FC<ImagesDisplayProps> = ({ imageUrls }) => {
   return (
     <div className="p-4 mx-16 mb-4">
       <h2 className="text-lg font-bold mb-4">15. FOTOS EXPLICATIVAS:</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div
+        className={`grid ${imageUrls.length === 2 ? 'grid-cols-1 gap-8' : 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'}`}
+      >
         {imageUrls.length > 0 ? (
           imageUrls.map((url, index) => (
             <div key={index} className="flex justify-center">
               <img
                 src={url}
                 alt={`Imagen ${index + 1}`}
-                className="max-w-full max-h-96 w-auto h-auto border-2 border-gray-300"
+                style={{
+                  width: imageUrls.length === 2 ? '100%' : '100%',
+                  height: imageUrls.length === 2 ? 'auto' : 'auto',
+                  maxHeight: imageUrls.length === 2 ? '600px' : '300px',
+                }}
+                className="border-2 border-gray-300"
                 crossOrigin="anonymous"
               />
             </div>
